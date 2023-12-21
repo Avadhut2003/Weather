1ee254d148d66650a1512fd34d7c99b5f64cef0b
zoro2030 <kingzoro2030@gmail.com>
2023-12-21 22:53:08 +0530

    First commit

--- app.js
+++ app.js
@@ -121,6 +121,6 @@ app.post("/",function(req,res){
 //    });
 //})
 
-app.listen(3000, function(){
-    console.log("At port 3000");
+app.listen(process.env.PORT ||3000, function(){
+    console.log("At port "+ process.env.PORT);
 });
